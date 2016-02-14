f492e5a5d68911bf30a0bcf0d76846e7ed869d14
Behrooz Yaghi <iranchild1@gmail.com>
2016-02-14 20:58:09 +0000

    Create tooti.lua

---
+++ plugins/tooti.lua
@@ -0,0 +1,25 @@
+
+local function run(msg, matches)
+  local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  return text
+end
+
+return {
+  description = "تکرار یک پیام",
+  usage = " تکرار پیام",
+  patterns = {
+    "^(.+)$"
+  }, 
+  run = run 
+}
+
+--Copyright and edit; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است
